7db7fa6700690d200c65126becac103d5826b850
amishaalazis <amishaalazis@yahoo.com>
2025-07-29 11:18:06 +0700

    push

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,8 +1,8 @@
-import { Auth } from '@supabase/auth-ui-react';
-import { ThemeSupa } from '@supabase/auth-ui-shared';
-import { supabase } from '../supabaseClient';
-import { useNavigate } from 'react-router-dom';
-import { useEffect } from 'react';
+import { Auth } from "@supabase/auth-ui-react";
+import { ThemeSupa } from "@supabase/auth-ui-shared";
+import { supabase } from "../supabaseClient";
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 
 const LoginPage = () => {
   const navigate = useNavigate();
@@ -10,9 +10,9 @@ const LoginPage = () => {
   // Efek ini akan memantau status autentikasi.
   // Jika pengguna berhasil login, ia akan langsung diarahkan ke halaman /admin.
   useEffect(() => {
-    supabase.auth.onAuthStateChange((event, session) => {
+    supabase.auth.onAuthStateChange((session) => {
       if (session) {
-        navigate('/admin');
+        navigate("/admin");
       }
     });
   }, [navigate]);
@@ -20,14 +20,18 @@ const LoginPage = () => {
   return (
     <div className="min-h-screen bg-rose-50 flex justify-center items-center p-4">
       <div className="w-full max-w-md bg-white p-8 rounded-2xl shadow-lg">
-        <h1 className="text-3xl font-bold font-serif text-rose-800 text-center mb-2">Welcome Back!</h1>
-        <p className="text-center text-slate-500 mb-8">Login to manage your blog.</p>
-        
+        <h1 className="text-3xl font-bold font-serif text-rose-800 text-center mb-2">
+          Welcome Back!
+        </h1>
+        <p className="text-center text-slate-500 mb-8">
+          Login to manage your blog.
+        </p>
+
         {/* Komponen Auth dari Supabase */}
         <Auth
           supabaseClient={supabase}
           appearance={{ theme: ThemeSupa }}
-          providers={['google']} 
+          providers={["google"]}
         />
       </div>
     </div>
